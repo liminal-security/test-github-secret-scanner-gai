bffa9f95ee5c3c8b3fadb6ee35eb2392b6d64aa4
gaientro <gai.lazar@entro.security>
2025-01-01 11:57:02 +0200

    add secret

---
+++ main.go
@@ -0,0 +1,9 @@
+package test_github_secret_scanner
+
+import "fmt"
+
+func main() {
+	fmt.Printf("hvs.Yi753NiLqZsDGISgrYVhzwq5")
+	fmt.Printf("ghp_Mer10tpSnrmwtypOhsD8CXNhOsp2fB3qHdfG")
+	fmt.Printf("ghp_Mer10tpSnrewtypOhsD8CXGhOsp2fB3qHdfG")
+}
