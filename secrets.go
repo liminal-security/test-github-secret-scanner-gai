0dd94ac35ef1c3dc130fef7e31c979be4cdf5cfb
gaientro <gai.lazar@entro.security>
2025-01-01 16:43:36 +0200

    add secret in branch 4

--- secrets.go
+++ secrets.go
@@ -10,6 +10,8 @@ func main() {
 	fmt.Printf("hvs.Yi753NiLqZsDGISgrYVhzwq5")
 	fmt.Printf("ghp_Mer10tpSnrmwtypOhsD8CXNhOsp2fB3qHdfG")
 	fmt.Printf("ghp_Mer10tpSnrewtypOhsD8CXGhOsp2fB3qHdfG")
+
+	fmt.Printf("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")
 }
 
 // comment ghp_dnznhYiU0KOMNS6i83HnvdIj7yOeLD4V
