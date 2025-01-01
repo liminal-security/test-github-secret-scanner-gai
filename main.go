547877f5c73c44d29e67258e54f8ee32fcc3cada
Gai Lazar <gail@jfrog.com>
2025-01-01 12:01:16 +0200

    Create main.go

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("ghp_Mer10tpSnrmwtypOhsD8CXNhOsp2fB3qQcoP")
+	fmt.Printf("AKIARYHCWRJF2TPDZTYF")
+	fmt.Printf("0KDdPw3hRcNHjhPjMvUp7TcDCyHJp06W1lwaLRty")
+	fmt.Printf("2fa2962a-9f49-417b-8912-bhr79941a7q5")
+	fmt.Printf("hvs.Yi753NiLqZsDGISgrYVhzwq5")
+	fmt.Printf("ghp_Mer10tpSnrmwtypOhsD8CXNhOsp2fB3qHdfG")
+	fmt.Printf("ghp_Mer10tpSnrewtypOhsD8CXGhOsp2fB3qHdfG")
+}
