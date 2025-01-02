177474e76b7934f7bbe120d475a4acb060546bf8
gaientro <gai.lazar@entro.security>
2025-01-02 11:52:15 +0200

    add secret 2

--- main.go
+++ main.go
@@ -7,4 +7,6 @@ func main() {
 	fmt.Printf("hvs.Yi753NiLqZsDGISgrYVhzwq5")
 	fmt.Printf("ghp_Mer10tpSnrmwtypOhsD8CXNhOsp2fB3qHdfG")
 	fmt.Printf("ghp_Mer10tpSnrewtypOhsD8CXGhOsp2fB3qHdfG")
+
+	fmt.Printf("ghp_Mer10tpSnrmwtypOhsD8CXNhOsp2fB3qQcoP")
 }
